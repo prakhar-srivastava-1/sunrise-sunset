1238fcd04605d5307a7c9f9fb774f78c97978215
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2021-08-10 04:53:44 +0530

    add main.py and logic

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+
+MY_LAT = 22.171841
+MY_LONG = 76.065422
+
+params = {
+    "lat": MY_LAT,
+    "long": MY_LONG
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=params)
+response.raise_for_status()
+response_data = response.json()
+print(f"Time of Sunrise - {response_data['results']['sunrise']}")
+print(f"Time of Sunset - {response_data['results']['sunset']}")
